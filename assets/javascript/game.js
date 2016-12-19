e571252c84b4974466bc318ef4ef4d9c2de9d15d
Tre' Anderson-Davis <macuser@MacUsers-MBP-2.domain_not_set.invalid>
2016-12-19 03:56:18 -0500

    working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,114 +1,74 @@
-// add global variables
+var Game ={
+	target: 0,
+	wins: 0,
+	losses: 0,
+	crystal1: 0,
+	crystal2: 0,
+	crystal3: 0,
+	crystal4: 0,
+	score: 0,
+	guess: function(event){
+		var choice = event.target.id;
+		Game.score += Game[choice];
 
-/*jslint browser: true*/ /*global  $*/
+		Game.update();
 
-$( document ).ready(function() {
+		if(Game.score > Game.target){
+			Game.losses++;
+			Game.reset();
+			alert("YOU LOST! :(");
+		}
 
-	var game = {
+		else if(Game.score === Game.target){
+		 	Game.wins++;
+		 	Game.reset();
+		 	alert("YOU WON! :)");
+		 }	
 
-		winningNumber : 0,
-
-		wins : 0,
-
-		losses : 0,
-
-		totalScore : 0,
-
-		creature1:0,
-
-		creature2:0,
-
-		creature3:0,
-
-		creature4:0,
-
-
-
-
+		 else {
+		 	Game.update
+		 }	
+		 	
 		
+	},
 
-		//startUp function
-		startUp: function(){
-			game.winningNumber = parseInt(Math.floor(Math.random() * (120 - 19 + 1)) + 19);
-
-			console.log("Computer picked " + game.winningNumber);
-
-			game.totalScore = 0;
-
-			$("#losses").html("Losses: " + game.losses);
-			$("#wins").html("Wins: " + game.wins);
-			$("#game-total").html(game.winningNumber);
-			$("#user-total").html(game.totalScore);
-
-			console.log("User's starting score " + game.totalScore);
-
-			//create a new array of 4 random numbers that will be used to assign the crystal's value
-
-
-			crystalNumbers = [];
-
-			crystalNumbers.length = 0;
-
-			
-
-			//set array value's
-			for (var crystalNumbers = [], i = 0; i < 4; i++) {
-				var numbers = parseInt(Math.floor( Math.random() * 11 +1).toString());
-				crystalNumbers.push(numbers);
-
-				// console.log(crystalNumbers);
-
-				$(".crystalSquare").each(function(i, obj) {
-					$(this).attr("data-crystalvalue", crystalNumbers[i]);
-					
-					//allows you to see array being created
-					console.log(parseInt($(this).data("crystalvalue")));	
-				});
-
-
-			}
-		},
-
-		scores: function() {
-			//on the click of the crystal's add value to the users total score
-			$(".crystalSquare").on('click', function(event){
-				game.totalScore = parseInt(game.totalScore) + parseInt($(this).data("crystalvalue"));
-
-				//see individual crystal values
-				console.log(parseInt($(this).data("crystalvalue")) + " is this crystal's value!");
-
-				//see the users total score	
-				console.log("This is the users total score " + game.totalScore);
-
-				$("#user-total").html(game.totalScore);
-
-				if (parseInt(game.totalScore) === game.winningNumber) {
-					parseInt(game.wins++);
-					$("#wins").html("Wins: " + game.wins);
-					$(this).removeData("crystalvalue"); // win or lose will reset values for new game
-					setTimeout(game.startUp, 1000);
-					setTimeout(game.scores, 1000);
-
-				} else if (parseInt(game.totalScore) > game.winningNumber) {
-					parseInt(game.losses++);
-					$("#losses").html("Losses: " + game.losses);
-					// console.log(game.losses + " losses!");
-					$(this).removeData("crystalvalue");
-					setTimeout(game.startUp, 1000);
-					setTimeout(game.scores, 1000); 
-				}
-				
-			});
-		} 
-
-		
-	};
+	update: function(){
+	$("#game-total").html(Game.target);
+	$("#wins").html("Wins: " + Game.wins);
+	$("#losses").html("Losses " + Game.losses);
+	$("#user-total").html(Game.score);
 	
-	game.startUp();
+	},
+	
+	reset: function(){
+	Game.target = Math.floor((Math.random() * 120) + 1);
+	$("#game-total").html(Game.target);
 
-	game.scores();
+	var arr = []
+	while(arr.length < 4){
+    var randomnumber = Math.ceil(Math.random()*12)
+    if(arr.indexOf(randomnumber) > -1) continue;
+    arr[arr.length] = randomnumber;
+    console.log(arr);
+	}
+
+	Game.crystal1 = arr[0];
+	Game.crystal2 = arr[1];
+	Game.crystal3 = arr[2];
+	Game.crystal4 = arr[3];
+	Game.score = 0;
+
+	Game.update();
+	},
+
+};
 
 
+document.addEventListener("DOMContentLoaded", function(){
 
+	Game.reset();
+	Game.update();
+
+	$('.crystalSquare').click(Game.guess);
 
 });
