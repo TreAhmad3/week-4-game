872a8d8b2b8cf198648aa6c0bec280b63de13f9d
Tre' Anderson-Davis <macuser@MacUsers-MBP-2.domain_not_set.invalid>
2016-12-18 21:33:03 -0500

    heroku

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,22 +18,101 @@
 
 $( document ).ready(function() {
 
-	var winningNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	var game = {
 
-	var wins = 0;
+		winningNumber : 0,
 
-	var losses = 0;
+		wins : 0,
 
-	var cryNumber = Math.floor( Math.random() * 12 + 1);
+		losses : 0,
 
-	var totalScore = 0;
+		totalScore : 0,
 
 
-	randomizer();
-	console.log(crystal1);
-	console.log(crystal2);
-	console.log(crystal3);
-	console.log(crystal4);
+
+		
+
+		//startUp function
+		startUp: function(){
+			winningNumber = parseInt(Math.floor(Math.random() * (120 - 19 + 1)) + 19);
+
+			console.log("Computer picked " + winningNumber);
+
+			totalScore = 0;
+
+			$("#losses").html("Losses: " + game.losses);
+			$("#wins").html("Wins: " + game.wins);
+			$("#game-total").html(winningNumber);
+			$("#user-total").html(totalScore);
+
+			console.log("User's starting score " + game.totalScore);
+
+			//create a new array of 4 random numbers that will be used to assign the crystal's value
+
+
+			crystalNumbers = [];
+
+			crystalNumbers.length = 0;
+
+			
+
+			//set array value's
+			for (var crystalNumbers = [], i = 0; i < 4; i++) {
+				var numbers = parseInt(Math.floor( Math.random() * 11 +1).toString());
+				crystalNumbers.push(numbers);
+
+				// console.log(crystalNumbers);
+
+				$(".crystalSquare").each(function(i, obj) {
+					$(this).attr("data-crystalvalue", crystalNumbers[i]);
+					
+					//allows you to see array being created
+					console.log(parseInt($(this).data("crystalvalue")));	
+				});
+
+
+			}
+		},
+
+		scores: function() {
+			//on the click of the crystal's add value to the users total score
+			$(".crystalSquare").on('click', function(event){
+				totalScore = parseInt(totalScore) + parseInt($(this).data("crystalvalue"));
+
+				//see individual crystal values
+				console.log(parseInt($(this).data("crystalvalue")) + " is this crystal's value!");
+
+				//see the users total score	
+				console.log("This is the users total score " + totalScore);
+
+				$("#user-total").html(totalScore);
+
+				if (parseInt(totalScore) === winningNumber) {
+					parseInt(game.wins++);
+					$("#wins").html("Wins: " + game.wins);
+					$(this).removeData("crystalvalue"); // win or lose will reset values for new game
+					setTimeout(game.startUp, 1000);
+					setTimeout(game.scores, 1000);
+
+				} else if (parseInt(totalScore) > winningNumber) {
+					parseInt(game.losses++);
+					$("#losses").html("Losses: " + game.losses);
+					// console.log(game.losses + " losses!");
+					$(this).removeData("crystalvalue");
+					setTimeout(game.startUp, 1000);
+					setTimeout(game.scores, 1000); 
+				}
+				
+			});
+		} 
+
+		
+	};
+	
+	game.startUp();
+
+	game.scores();
+
 
 
 
